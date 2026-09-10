16f45b3073c786cdd157f9b7a17bde5eebfcdf71
alpereira-mpc <alpereira@tce.pb.gov.br>
2026-09-10 06:36:27 -0300

    Update portal.py

--- portal.py
+++ portal.py
@@ -46,7 +46,7 @@ MODULES = (
     Module(
         "agenda",
         "Agenda",
-        "Agenda dos Procuradores (em teste)",
+        "Agenda dos Procuradores",
         "Organização de eventos, reuniões e despachos dos procuradores.",
         "calendar_month",
         True,
